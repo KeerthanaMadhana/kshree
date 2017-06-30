d93f3d6109916972d90f64f3a76d2dcac723e801
KeerthanaMadhana <keerthana19963071@gmail.com>
2017-06-30 12:00:21 +0530

    Create reversestring.java

---
+++ reversestring.java
@@ -0,0 +1,16 @@
+package keerthan;
+
+public class Reversestring {
+	public static void main(String[] args) {
+		String str = "keerthanashree";
+		 
+		char[] strArray = str.toCharArray();
+		 
+		for (int i = strArray.length - 1; i >= 0; i--)
+		{
+		    System.out.print(strArray[i]);    
+		}
+
+	}
+
+}
